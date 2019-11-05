71c3690f55d20225555893ac13127f8289f08ec5
ChickenAhn <chiwanan91@gmail.com>
2019-11-05 15:33:16 -0500

    Add example code

--- src/App.js
+++ src/App.js
@@ -9,6 +9,17 @@ function App() {
   const [creating, setCreating] = useState(false)
   const [deleting, setDeleting] = useState(false)
   const [badges, setBadges] = useState([])
+  const [isCoppied, setCoppied] = useState(false)
+
+  // clear isCoppied state
+  React.useEffect(() => {
+    const timer = setTimeout(() => setCoppied(false), 1000)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [isCoppied])
+
+  const exampleCode = `<img style="width:414px" src="https://firebasestorage.googleapis.com/v0/b/mobbin-15894.appspot.com/o/patterns%2Fnetflix_v11.22.0_18.PNG?alt=media&amp;token=d9e46463-06a5-4bda-a1c5-8845d3fdd0cc">`
 
   const handleChange = e => {
     setSvgCode(e.target.value)
@@ -48,6 +59,16 @@ function App() {
     setBadges(prev => prev.filter(item => item.id !== id))
   }
 
+  const handleCopy = () => {
+    const textField = document.createElement('textarea')
+    textField.innerText = exampleCode
+    document.body.appendChild(textField)
+    textField.select()
+    document.execCommand('copy')
+    textField.remove()
+    setCoppied(true)
+  }
+
   return (
     <div
       className="App"
@@ -81,12 +102,41 @@ function App() {
             placeholder="Paste Your SVG Code here..."
           ></textarea>
         </div>
-        <div onClick={handleClick}>
-          <div
-            className={clsx('svgContainer', creating && 'active')}
-            dangerouslySetInnerHTML={{ __html: svgCode }}
-          ></div>
-        </div>
+        {svgCode ? (
+          <>
+            <div class="description">
+              <p>
+                Add label <kbd>cmd</kbd> or <kbd>ctrl</kbd> + <kbd>click</kbd>
+              </p>
+              <p>
+                Remove label <kbd>alt</kbd> + <kbd>click</kbd>
+              </p>
+            </div>
+            <div onClick={handleClick}>
+              <div
+                className={clsx('svgContainer', creating && 'active')}
+                dangerouslySetInnerHTML={{ __html: svgCode }}
+              ></div>
+            </div>
+          </>
+        ) : (
+          <div>
+            <h3>
+              <span role="img" aria-label="describing-emoji">
+                💁
+              </span>
+              ‍Here is sample image code
+            </h3>
+            <div class="exampleContainer">
+              <div class="example">{exampleCode}</div>
+              <div class="overlay">
+                <button class="overlayButton" onClick={handleCopy}>
+                  {isCoppied ? 'COPPIED!! 🔥' : 'COPY CODE'}
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
